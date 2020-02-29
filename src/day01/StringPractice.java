1d4b185258c008900f17cd4c52f3c2cd29e172b6
SCybertek <snuryyeva@gmail.com>
2020-02-29 18:24:31 -0500

    update1

--- src/day01/StringPractice.java
+++ src/day01/StringPractice.java
@@ -7,6 +7,7 @@ public class StringPractice {
         System.out.println("Hello, world!");
         System.out.println("I love Java");
         System.out.println("this line should be green");
+        System.out.println("Ainura practice ");
 
     }
 }
