c2a23346780371ea4fac4765138c7631a3273e1c
SCybertek <58412982+SCybertek@users.noreply.github.com>
2020-03-12 16:12:55 -0400

    Update Colors.java

--- src/day01/Colors.java
+++ src/day01/Colors.java
@@ -7,5 +7,7 @@ public class Colors {
         String [] colors = {"red", "blue", "green","yellow"};
 
         System.out.println("My Colors");
+        
+        System.out.println("This line was updated from GItHUB");
     }
 }
